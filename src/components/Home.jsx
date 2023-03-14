ee90a120a65ca3a1e08205ad6177c9e565ce1de6
vivek singh <vickydigari6154@gmail.com>
2023-03-14 14:50:57 +0530

    style: added data in the home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-3.5xl sm:text-7xl font-bold text-white">
+          <h2 className="text-3xl sm:text-7xl font-bold text-white">
             I'm a frontend developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
