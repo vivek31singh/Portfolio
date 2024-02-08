77ed5db264bf8344f286d8f108e700846d52a103
vivek singh <vickydigari6154@gmail.com>
2024-02-09 01:01:25 +0530

    fixed a deployment bug

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -80,11 +80,11 @@ const Portfolio = () => {
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   {" "}
-                  <a href={href1} target="_blank">Demo</a>
+                  <a href={href1} target="_blank" rel="noreferrer">Demo</a>
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   {" "}
-                  <a href={href2} target="_blank">Code</a>
+                  <a href={href2} target="_blank" rel="noreferrer">Code</a>
                 </button>
               </div>
             </div>
