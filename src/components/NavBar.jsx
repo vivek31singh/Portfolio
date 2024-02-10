3d010c23fab4ec2ef71b393892eaa345270e3881
vivek singh <vickydigari6154@gmail.com>
2024-02-10 19:17:18 +0530

    fixed all the responsivess issues

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -33,7 +33,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 text-white fixed bg-black px-4 ">
+    <div className="flex justify-between items-center w-full h-20 text-white fixed z-10 bg-black px-4 ">
       <div>
         <h1 className="text-5xl font-signature ml-2">Vivek</h1>
       </div>
